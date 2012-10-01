f5911e5a4765e559e4090ead829a30a946629711
Wesley <Wesley@10.30.16.193>
2012-10-01 15:16:40 -0500

    Labs

---
+++ src/dip/lab2/CalculateTipService.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dip.lab2;
+
+import dip.lab1.instructor.solution1.Employee;
+
+/**
+ *
+ * @author wsoehrmann
+ */
+public class CalculateTipService {
+    private TipStratagy tip;
+
+  public CalculateTipService(TipStratagy t) {
+        tip = t;
+    }
+    
+}
---
+++ src/dip/lab2/TipStratagy.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dip.lab2;
+
+/**
+ *
+ * @author wsoehrmann
+ */
+public interface TipStratagy {
+    public abstract double getTip();
+}
